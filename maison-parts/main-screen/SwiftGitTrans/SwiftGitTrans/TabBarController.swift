8bda8946235251b61464ade212ab9feba6ff6cbf
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-05-15 11:12:15 +0900

    SwiftGitTransの動きの変更。新しいMainStoryboardを追加できるように設定

--- maison-parts/main-screen/SwiftGitTrans/SwiftGitTrans/TabBarController.swift
+++ maison-parts/main-screen/SwiftGitTrans/SwiftGitTrans/TabBarController.swift
@@ -24,21 +24,23 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
 			UIImage(named: "person")!,
 			UIImage(named: "chatroom")!,
 			UIImage(named: "Path")!,
-			UIImage(named: "gear")!],
+			//UIImage(named: "gear")!
+            ],
 			colors: [
 				UIColor(red: 240/255, green: 159/255, blue: 254/255, alpha: 1),
 				UIColor(red: 255/255, green: 137/255, blue: 167/255, alpha: 1),
 				UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
 				UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1),
-				UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1)],
+				// UIColor(red: 126/255, green: 242/255, blue: 195/255, alpha: 1)
+                ],
 			selectionStyle: .single)
 		sidebar.actionForIndex = [
 			0: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 0}) },
 			1: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 1}) },
 			2: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 2}) },
 			3: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 3}) },
-			4: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 4}) },
-			5: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 5}) }
+			// 4: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 4}) },
+			// 5: {self.sidebar.dismissAnimated(true, completion: { finished in self.selectedIndex = 5}) }
             ]
 	}
 	
