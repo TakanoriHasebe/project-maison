479f74593e9287e896e57caf5dcf7d242afc8ee3
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-06-23 15:33:18 +0900

    PopUpViewに対してコードでprofileImageを追加

--- maison-parts/recommend-person/PopUpView/PopUpView/PopUpViewController.swift
+++ maison-parts/recommend-person/PopUpView/PopUpView/PopUpViewController.swift
@@ -10,24 +10,88 @@ import UIKit
 
 class PopUpViewController: UIViewController {
 
+    /******************** create UI ********************/
+    
+    /* 自己紹介の画像 */
+    let profileImage: UIImageView = {
+       
+        let profileImage = UIImageView()
+        profileImage.image = UIImage(named: "personImage0")
+        profileImage.translatesAutoresizingMaskIntoConstraints = false
+        profileImage.contentMode = .scaleAspectFill
+        
+        return profileImage
+        
+    }()
+    
+    /* 名前の文 */
+    let nameLabel: UILabel = {
+        
+        let nameLabel = UILabel()
+        nameLabel.textColor = UIColor.white
+        nameLabel.backgroundColor = UIColor.black
+        nameLabel.text = "Mick Willoughby"
+        nameLabel.sizeToFit()
+        
+        return nameLabel
+        
+    }()
+    
+    /* 自己紹介の文 */
+    let profileLabel: UILabel = {
+       
+        let profileLabel = UILabel()
+        profileLabel.backgroundColor = UIColor.black
+        profileLabel.textColor = UIColor.white
+        profileLabel.text = "Test"
+        profileLabel.font = UIFont(name: "HiraMinProN-W3", size: 15)
+        profileLabel.sizeToFit()
+        
+        return profileLabel
+        
+    }()
+    
+    /******************** call first ********************/
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
         
-        let label = UILabel()
-        // label.frame = CGRect(x:150, y:300, width:100, height:100)
-        label.text = "Test"
-        label.backgroundColor = UIColor.white
-        label.textColor = UIColor.black
-        self.view.addSubview(label)
+        self.view.backgroundColor = UIColor.black
         
-        let screenWidth:CGFloat = view.frame.size.width
-        label.frame = CGRect(x:10, y:300, width:screenWidth-20, height:100)
+        /* 各UIの貼り付け */
+        view.addSubview(profileImage)
+        view.addSubview(nameLabel)
         
-        label.textAlignment = .right
+        /* constraintsの設定 */
+        setupProfileImage()
+        setupProfileName()
+        
         
         self.showAnimate()
+        
+    }
+    
+    /******************** contraints ********************/
+    
+    /* プロフィールの画像 */
+    func setupProfileImage(){
+        // need x, y, width, hegiht, constraints
+        profileImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        profileImage.widthAnchor.constraint(equalToConstant: view.bounds.size.width).isActive = true
+        profileImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    }
+    
+    /* プロフィールの名前 */
+    func setupProfileName(){
+        // need x, y, width, hegiht, constraints
+        nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 100).isActive = true
+        
+    }
+    
+    /* プロフィールの文 */
+    func setupProfileLabel(){
+        // need x, y, width, hegiht, constraints
+        
         
     }
 
@@ -71,3 +135,19 @@ class PopUpViewController: UIViewController {
     }
     
 }
+
+/* 残り書き */
+/*
+ /* UILabelをコードで書いている */
+ let label = UILabel()
+ label.frame = CGRect(x:150, y:300, width:100, height:100)
+ label.text = "In my free time I like reading, going out with friends, listening to music, and going for walks around Tokyo."
+ label.backgroundColor = UIColor.white
+ label.textColor = UIColor.black
+ /* Labelのフォントを変更 */
+ label.font = UIFont(name: "HiraMinProN-W3", size: 15)
+ label.sizeToFit()
+ self.view.addSubview(label)
+ */
+
+
