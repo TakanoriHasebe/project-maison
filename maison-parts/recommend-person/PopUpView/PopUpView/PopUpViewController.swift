05171ab9c0b5b4f1a9fc265d1887b5d60e68ea66
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-06-28 11:25:42 +0900

    PopUpViewのデザインの変更

--- maison-parts/recommend-person/PopUpView/PopUpView/PopUpViewController.swift
+++ maison-parts/recommend-person/PopUpView/PopUpView/PopUpViewController.swift
@@ -13,16 +13,24 @@ class PopUpViewController: UIViewController {
     
     @IBOutlet weak var profileLabel: UILabel!
     
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+        self.removeAnimate()
+        
+    }
+    
+    
     /* Main.storyboardを用いた場合には、画像の読み込み、自己紹介の読み込みについて */
+    
     /******************** call first ********************/
     override func viewDidLoad() {
         super.viewDidLoad()
         
         /* 背景色の設定 */
-        // self.view.backgroundColor = UIColor.black
-        profileLabel.text = "A complex sentence has a main clause and one or more adverbial clauses. Adverbial clauses usually come after the main clause"
+        profileLabel.text = "was a cold December day when I chose the comfort of my bed over the hype of another Silicon Valley Christmas party. I spent the evening browsing the catalogue of men on Tinder, wondering if my soulmate could actually be a finger swipe away. When M’s profile came up my heart skipped a beat."
         
         
+        /* アニメーションの設定 */
         self.showAnimate()
         
     }
@@ -63,97 +71,3 @@ class PopUpViewController: UIViewController {
     }
     
 }
-
-/* 残り書き */
-/*
- /* UILabelをコードで書いている */
- let label = UILabel()
- label.frame = CGRect(x:150, y:300, width:100, height:100)
- label.text = "In my free time I like reading, going out with friends, listening to music, and going for walks around Tokyo."
- label.backgroundColor = UIColor.white
- label.textColor = UIColor.black
- /* Labelのフォントを変更 */
- label.font = UIFont(name: "HiraMinProN-W3", size: 15)
- label.sizeToFit()
- self.view.addSubview(label)
- */
-
-/*
- /******************** create UI ********************/
- 
- /* 自己紹介の画像 */
- let profileImage: UIImageView = {
- 
- let profileImage = UIImageView()
- profileImage.image = UIImage(named: "personImage0")
- profileImage.translatesAutoresizingMaskIntoConstraints = false
- profileImage.contentMode = .scaleAspectFill
- 
- return profileImage
- 
- }()
- 
- /* 名前の文 */
- let nameLabel: UILabel = {
- 
- let nameLabel = UILabel()
- nameLabel.textColor = UIColor.white
- nameLabel.backgroundColor = UIColor.black
- nameLabel.text = "Mick Willoughby"
- nameLabel.sizeToFit()
- 
- return nameLabel
- 
- }()
- 
- /* 自己紹介の文 */
- let profileLabel: UILabel = {
- 
- let profileLabel = UILabel()
- profileLabel.backgroundColor = UIColor.black
- profileLabel.textColor = UIColor.white
- profileLabel.text = "Test"
- profileLabel.font = UIFont(name: "HiraMinProN-W3", size: 15)
- profileLabel.sizeToFit()
- 
- return profileLabel
- 
- }()
- */
-
-/*
- /******************** contraints ********************/
- 
- /* プロフィールの画像 */
- func setupProfileImage(){
- // need x, y, width, hegiht, constraints
- profileImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
- profileImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
- profileImage.widthAnchor.constraint(equalToConstant: view.bounds.size.width).isActive = true
- profileImage.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
- }
- 
- /* プロフィールの名前 */
- func setupProfileName(){
- // need x, y, width, hegiht, constraints
- // nameLabel.topAnchor.constraint(equalTo: profileImage.bottomAnchor, constant: 100).isActive = true
- 
- }
- 
- /* プロフィールの文 */
- func setupProfileLabel(){
- // need x, y, width, hegiht, constraints
- 
- 
- }
- */
-
-/*
- /*
- @IBAction func closePopUp(_ sender: AnyObject) {
- 
- // self.view.removeFromSuperview()
- self.removeAnimate()
- 
- }*/
- */
