b5a76279bea6981dbc688e22b0ddb025c45b4c65
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-07-27 16:04:20 +0900

    PopUpとUITabBarを用いた時のCollectionViewについて

---
+++ maison-parts/recommend-person/SwiftBlurryPopUpView/SwiftBlurryPopUpView/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  SwiftBlurryPopUpView
+//
+//  Created by Takanori.H on 2017/07/27.
+//  Copyright © 2017年 Takanori.H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var addItemView: UIView!
+    
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    
+    var effect: UIVisualEffect!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        effect = visualEffectView.effect
+        visualEffectView.effect = nil
+        
+        addItemView.layer.cornerRadius = 5
+        
+    }
+    
+    func animateIn(){
+        
+        self.view.addSubview(addItemView)
+        
+        addItemView.center = self.view.center
+        
+        addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addItemView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) {
+            self.visualEffectView.effect = self.effect
+            self.addItemView.alpha = 1
+            self.addItemView.transform = CGAffineTransform.identity
+        }
+
+        
+    }
+    
+    @IBAction func addItem(_ sender: AnyObject) {
+        
+        animateIn()
+        
+    }
+    
+    @IBAction func dismissPopUp(_ sender: AnyObject) {
+        
+        
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+        
+    }
+
+
+}
+
--- maison-parts/recommend-person/SwiftTabBarCollection/SwiftTabBarCollection/ViewController.swift
+++ maison-parts/recommend-person/SwiftTabBarCollection/SwiftTabBarCollection/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIGestureRecognizerDelegate{
     @IBOutlet weak var collectionView: UICollectionView!
     
     var images = ["one", "two", "three", "four", "five", "six"]
@@ -16,9 +16,51 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /* Collection Viewに必要 */
         collectionView.delegate = self
         collectionView.dataSource = self
         
+        /* LongPress */
+        let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.handleLongPress(_:)))
+        lpgr.minimumPressDuration = 0.3
+        lpgr.delaysTouchesBegan = true
+        lpgr.delegate = self
+        self.collectionView.addGestureRecognizer(lpgr)
+        
+        self.collectionView.isUserInteractionEnabled = true
+        
+    }
+    
+    /* LongPressの時の動作 */
+    func handleLongPress(_ gestureReconizer: UILongPressGestureRecognizer) {
+        /*
+         if gestureReconizer.state != UIGestureRecognizerState.ended {
+         return
+         }*/
+        
+        let p = gestureReconizer.location(in: collectionView)
+        let indexPath = collectionView.indexPathForItem(at: p)
+        
+        if let index = indexPath {
+            var cell = collectionView.cellForItem(at: index)
+            // do stuff with your cell, for example print the indexPath
+            
+            if( gestureReconizer.state == UIGestureRecognizerState.began) {
+                print("Long Press Begin")
+                let transform = self.collectionView.transform
+                UICollectionView.animate(withDuration: 0.0, animations: {
+                    cell?.transform = CGAffineTransform(scaleX: 0.98, y: 0.98)
+                })
+                print(index.row)
+            }else if (gestureReconizer.state == UIGestureRecognizerState.ended) {
+                cell?.transform = CGAffineTransform.identity
+                print("Long Press Over")
+            }
+            
+            
+        } else {
+            print("Could not find index path")
+        }
     }
 
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
