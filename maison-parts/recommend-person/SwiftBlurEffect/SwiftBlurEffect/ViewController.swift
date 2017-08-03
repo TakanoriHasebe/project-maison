6938624b3a2e0a01baf7cc9a1baa341fc66b0070
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-08-03 14:00:10 +0900

    Blurを@IBOutletにする前

--- maison-parts/recommend-person/SwiftBlurEffect/SwiftBlurEffect/ViewController.swift
+++ maison-parts/recommend-person/SwiftBlurEffect/SwiftBlurEffect/ViewController.swift
@@ -12,49 +12,57 @@ class ViewController: UIViewController {
 
     @IBOutlet var addItemView: UIView!
     
-        
     var effect:UIVisualEffect!
     
     /* Effectボタンを押すとBlurがでる */
     @IBAction func BlurEffect(_ sender: Any) {
         
-        
+        /*
+        /************ コピペ ***********/
         /* BlurEffectの初期設定 */
-        /* コピペ */
         let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
         let blurView = UIVisualEffectView(effect: blurEffect)
+        /* Blurのかける範囲 */
         blurView.frame.size = CGSize(width: self.view.layer.bounds.width, height: self.view.layer.bounds.height)
+        /* どこに中央を設定するか */
         blurView.center = view.center
         // view.addSubview(blurView)
-        /* コピペ */
+        /************ コピペ ***********/
+        */
         
         
+        /************ コピペ ***********/
         /* addItemViewの初期設定 */
-        /* コピペ */
         addItemView.center = self.view.center
         // self.view.addSubview(addItemView)
-        /* コピペ */
+        /************ コピペ ***********/
         
+        
+        /************ コピペ ***********/
+        /* Animationの設定 */
         addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
         addItemView.alpha = 0
         
         UIView.animate(withDuration: 0.4) {
-            self.view.addSubview(blurView)
+            // self.view.addSubview(blurView)
             self.view.addSubview(self.addItemView)
             self.addItemView.alpha = 1
             self.addItemView.transform = CGAffineTransform.identity
         }
+        /************ コピペ ***********/
         
     }
     
+    /* BlurエフェクトとViewを取り外す */
     @IBAction func removeBlurEffect(_ sender: Any) {
         
         UIView.animate(withDuration: 0.3, animations: {
             self.addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
             self.addItemView.alpha = 0
             
-            
         }) { (success:Bool) in
+            
+            
             self.addItemView.removeFromSuperview()
             
         }
