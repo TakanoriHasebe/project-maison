d4725fe21a1a9a6503a2c5c5958422bc5289f333
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-12-10 12:01:52 +0900

    最初のログインについて追加

---
+++ programs/SwiftImperialLogin/SwiftImperialLogin/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SwiftImperialLogin
+//
+//  Created by Takanori.H on 2017/12/10.
+//  Copyright © 2017年 Takanori.H. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
