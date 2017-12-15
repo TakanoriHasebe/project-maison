e9a1fcf33024af337296e93bbf7d3820b064f862
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-12-15 11:43:03 +0900

    Profile画面のAutoLayoutとScrollViewの概要まで追加

---
+++ maison-parts/recommend-person/AutoLayoutAdaptiveLabel/AutoLayoutAdaptiveLabel/README.swift
@@ -0,0 +1,22 @@
+//
+//  readme.swift
+//  AutoLayoutAdaptiveLabel
+//
+//  Created by Takanori.H on 2017/12/15.
+//  Copyright © 2017年 Takanori.H. All rights reserved.
+//
+
+import Foundation
+
+/*************
+ - Scroll ViewについてのURL
+ https://www.youtube.com/watch?v=LhhW3xqhCzg
+ https://qiita.com/fromage-blanc/items/d480f2a45f0f60474531
+ **************/
+
+/*************
+- UILabelのサイズを文字数によって動的に変更
+https://www.youtube.com/watch?v=BnD2XMqDhfI
+**************/
+
+
