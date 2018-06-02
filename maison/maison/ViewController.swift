5dc460f866666d7f467e415e2218d99d374bd642
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2018-06-02 11:01:42 +0900

    temp

--- maison/maison/ViewController.swift
+++ maison/maison/ViewController.swift
@@ -27,7 +27,6 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate, GIDSignInUIDel
         GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
         GIDSignIn.sharedInstance().delegate = self
         
-        
     }
     
     // Google
